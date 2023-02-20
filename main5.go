94bc81c1960bb59607b5f0625c75d18eb5b2cffb
kangxuan <895560759@qq.com>
2023-02-20 16:30:01 +0800

    gin 模板自定义函数

---
+++ main5.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	// 定义模板函数，可以在模板中使用
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML {
+			return template.HTML(str)
+		},
+	})
+	// 定义模板
+	// 解析模板
+	r.LoadHTMLFiles("main5Templates/index.tmpl")
+	r.GET("/main5/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", "<a href='https://baidu.com'>百度</a>") // 渲染模板
+	})
+	_ = r.Run(":9090")
+}
