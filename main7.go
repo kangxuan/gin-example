0b3b220dfd7ba829da75574615727e125858bb6e
kangxuan <895560759@qq.com>
2023-02-20 20:12:17 +0800

    模板渲染

---
+++ main7.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	// 定义静态文件的地址
+	r.Static("/xxx", "./main7Templates/statics")
+	// 解析模板
+	r.LoadHTMLFiles("./main7Templates/index.html")
+	r.GET("/main7/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil) // 渲染模板
+	})
+
+	_ = r.Run(":9090")
+}
